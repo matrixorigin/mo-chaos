fd7857aac4a9972275680fe8ef312c116a07e63d
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-10-17 10:53:08 +0800

    parameterize

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM python:3.12
+WORKDIR /root
+COPY . .
+RUN pip install -r requirements.txt
+ENTRYPOINT python main.py
--- litmus/const.py
+++ litmus/const.py
@@ -1,7 +1,9 @@
+import os
+
 from litmus.auth import get_auth_token
 
-# TODO should be parametrized, just temporary
-url = 'localhost:9091'
-username = 'admin'
-password = 'litmus'
+# should be parametrized
+url = os.getenv('LITMUS_URL')
+username = os.getenv('LITMUS_USERNAME')
+password = os.getenv('LITMUS_PASSWORD')
 token = get_auth_token(url, username, password)
