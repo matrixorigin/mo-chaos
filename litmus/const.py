1fad26459fcef6a96d8fe6ce97a96839fe1006f3
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-10-25 18:56:28 +0800

    fix

--- litmus/const.py
+++ litmus/const.py
@@ -4,6 +4,6 @@ from litmus.auth import get_auth_token
 
 # should be parametrized
 url = os.getenv('LITMUS_URL') or 'chaos-litmus-frontend-service.litmus:9091'
-username = os.getenv('LITMUS_USERNAME') or 'admit'
+username = os.getenv('LITMUS_USERNAME') or 'admin'
 password = os.getenv('LITMUS_PASSWORD') or 'litmus'
 token = get_auth_token(url, username, password)
