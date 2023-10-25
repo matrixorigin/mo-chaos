a62967ecb1210b91ff8001b96e845b7e5b718e1f
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-10-25 18:48:17 +0800

    add some debug info

--- litmus/auth.py
+++ litmus/auth.py
@@ -6,4 +6,7 @@ def get_auth_token(litmus_url: str, username: str, password: str) -> str:
         "username": username,
         "password": password
     }
-    return requests.post(f"http://{litmus_url}/auth/login", json=payload).json()["access_token"]
+    print(litmus_url)
+    resp = requests.post(f"http://{litmus_url}/auth/login", json=payload)
+    print(resp.status_code)
+    return resp.json()["access_token"]
