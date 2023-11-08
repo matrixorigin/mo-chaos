dc8bef6e0a4d4bffa514390d2e2ceaa7f5f27d7b
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-08 11:35:34 +0800

    test

--- .github/workflows/clean-mo-tke.sh
+++ .github/workflows/clean-mo-tke.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 NAMESPACE=$1
-# create namespace
-#kubectl create ns $NAMESPACE
-## create minio secret
-#kubectl apply -n $NAMESPACE -f s3-secret.yaml
-## create mo cluster
-#kubectl apply -n $NAMESPACE -f mo-cluster.yaml
-ls
-pwd
+# delete namespace
+kubectl delete ns $NAMESPACE
--- .github/workflows/install-mo-tke.sh
+++ .github/workflows/install-mo-tke.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 NAMESPACE=$1
 # create namespace
-#kubectl create ns $NAMESPACE
+kubectl create ns $NAMESPACE
 ## create minio secret
 #kubectl apply -n $NAMESPACE -f s3-secret.yaml
 ## create mo cluster
 #kubectl apply -n $NAMESPACE -f mo-cluster.yaml
-ls
-pwd
