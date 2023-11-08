18a39d1918632756eb35006eb42d71d324cf687b
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-08 14:47:34 +0800

    test

--- .github/workflows/install-mo-tke.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-NAMESPACE=$1
-# create namespace
-kubectl create ns $NAMESPACE
-## create minio secret
-#kubectl apply -n $NAMESPACE -f s3-secret.yaml
-## create mo cluster
-#kubectl apply -n $NAMESPACE -f mo-cluster.yaml
---
+++ .github/workflows/tke/install-mo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+NAMESPACE=$1
+# create namespace
+kubectl create ns $NAMESPACE
+# create secret
+kubectl create secret generic tencent-token -n $NAMESPACE --from-literal=AWS_ACCESS_KEY_ID='${{secrets.COS_AK}}' --from-literal=AWS_SECRET_ACCESS_KEY='${{secrets.COS_SK}}'
+# create mo cluster file
+appName=$(cat /proc/sys/kernel/random/uuid)
+imageTag=$(curl https://hub.docker.com/v2/namespaces/matrixorigin/repositories/matrixone/tags|jq -r '.results[0].name')
+eval "echo \"$(cat .github/workflows/tke/mo-cluster.temp)\""
+eval "echo \"$(cat .github/workflows/tke/mo-cluster.temp)\"" > mo-cluster.yaml
+# create mo cluster
+kubectl apply -n $NAMESPACE -f mo-cluster.yaml
