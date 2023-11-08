c29f652f68de45562c67f93d068e8a4373e56a2f
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-08 11:10:40 +0800

    add new pipeline

---
+++ .github/workflows/install-mo-tke.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+NAMESPACE=$1
+# create namespace
+#kubectl create ns $NAMESPACE
+## create minio secret
+#kubectl apply -n $NAMESPACE -f s3-secret.yaml
+## create mo cluster
+#kubectl apply -n $NAMESPACE -f mo-cluster.yaml
+ls
+pwd
