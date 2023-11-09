a73918f13572ed4cdb57edb78da41582ba7ab85f
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-09 16:11:40 +0800

    test

--- .github/workflows/tke/clean-mo.sh
+++ .github/workflows/tke/clean-mo.sh
@@ -4,7 +4,9 @@ uuid=$(cat uuid)
 NAMESPACE=chaos-$uuid
 # remove mo clusterr
 echo "kubectl delete -f mo-cluster.yaml -n $NAMESPACE"
-kubectl delete -f mo-cluster.yaml -n $NAMESPACE
+#kubectl delete -f mo-cluster.yaml -n $NAMESPACE
 # delete namespace
 echo "kubectl delete ns $NAMESPACE"
-kubectl delete ns $NAMESPACE
+#kubectl delete ns $NAMESPACE
+ls
+pwd
