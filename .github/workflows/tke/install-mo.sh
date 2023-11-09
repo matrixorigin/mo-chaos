cbe52843020a36698b6f87b06f5444f064fa6162
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-09 17:07:44 +0800

    test

--- .github/workflows/tke/install-mo.sh
+++ .github/workflows/tke/install-mo.sh
@@ -6,7 +6,7 @@ NAMESPACE=chaos-$uuid
 # create namespace
 kubectl create ns $NAMESPACE
 # create secret
-kubectl create secret generic tencent-token -n $NAMESPACE --from-literal=AWS_ACCESS_KEY_ID='${{secrets.COS_AK}}' --from-literal=AWS_SECRET_ACCESS_KEY='${{secrets.COS_SK}}'
+kubectl create secret generic tencent-token -n $NAMESPACE --from-literal=AWS_ACCESS_KEY_ID='$COS_AK' --from-literal=AWS_SECRET_ACCESS_KEY='$COS_SK'
 # create mo cluster file
 appName=$uuid
 imageTag=$(curl https://hub.docker.com/v2/namespaces/matrixorigin/repositories/matrixone/tags|jq -r '.results[0].name')
