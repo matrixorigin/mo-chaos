016de449a992d79d9355cd367f15657c2b460b06
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-10 17:37:00 +0800

    test

--- litmus/scenarios/cn_pod_delete.py
+++ litmus/scenarios/cn_pod_delete.py
@@ -6,5 +6,6 @@ class CnPodDelete:
         self.name = 'cn-pod-delete'
         self.duration = os.getenv('DURATION') or '60'
         self.interval = os.getenv('INTERVAL') or '10'
-        self.namespace = os.getenv('NAMESPACE') or 'mo-cluster'
+        self.namespace = os.getenv('NAMESPACE') or open(
+            '/var/run/secrets/kubernetes.io/serviceaccount/namespace').read()
         self.label = os.getenv('LABEL') or 'matrixorigin.io/component=CNSet'
