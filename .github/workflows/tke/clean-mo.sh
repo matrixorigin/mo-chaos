8ef836c03cf7ee1163cc9af8af07ece5fff1ee8d
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-13 14:50:13 +0800

    test

--- .github/workflows/tke/clean-mo.sh
+++ .github/workflows/tke/clean-mo.sh
@@ -5,7 +5,7 @@ uuid=$(cat uuid)
 NAMESPACE=chaos-$uuid
 # remove mo clusterr
 echo "kubectl delete -f mo-cluster.yaml -n $NAMESPACE"
-kubectl delete -f mo-cluster.yaml -n $NAMESPACE
+kubectl delete matrixoneclusters.core.matrixorigin.io chaos -n $NAMESPACE
 # delete namespace
 echo "kubectl delete ns $NAMESPACE"
 kubectl delete ns $NAMESPACE
