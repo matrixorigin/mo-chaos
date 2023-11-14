c6fd866c500f8d7b26e2a1564d98416c40fedc79
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-14 14:33:26 +0800

    test

--- .github/workflows/tke/clean-mo.sh
+++ .github/workflows/tke/clean-mo.sh
@@ -3,19 +3,20 @@ set -e
 ls
 name=$(cat name)
 NAMESPACE=chaos-$name
+# remove mo clusterr
+echo "kubectl delete matrixoneclusters.core.matrixorigin.io $name -n $NAMESPACE"
+kubectl delete matrixoneclusters.core.matrixorigin.io $name -n $NAMESPACE
+# wait for it
 while [ 1 ]
 do
   res=$(kubectl get bucketclaims.core.matrixorigin.io -n $NAMESPACE --ignore-not-found)
-  if [[ $res -eq "" ]]
+  if [[ -z $res ]]
   then
     break
   else
     sleep 1
   fi
 done
-# remove mo clusterr
-echo "kubectl delete matrixoneclusters.core.matrixorigin.io $name -n $NAMESPACE"
-kubectl delete matrixoneclusters.core.matrixorigin.io $name -n $NAMESPACE
 # delete namespace
 echo "kubectl delete ns $NAMESPACE"
 kubectl delete ns $NAMESPACE
