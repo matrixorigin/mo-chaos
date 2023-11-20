be9c06aa7be9e219dd57779b8d83a390dbeabe8d
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-20 18:27:17 +0800

    test

--- .github/workflows/tke/prepare-cn-data.sh
+++ .github/workflows/tke/prepare-cn-data.sh
@@ -4,6 +4,7 @@ NAMESPACE=chaos-$name
 
 for pod in $(kubectl get pods --selector=matrixorigin.io/component=CNSet -n $NAMESPACE --no-headers -o custom-columns=":metadata.name"); do
   kubectl exec -it $pod -n $NAMESPACE -- bash -c '
+  export https_proxy=http://proxy-service.proxy.svc.cluster.local:8001
   git clone https://github.com/matrixorigin/matrixone
   '
 done
