9d9175b7ddbacf0175528c2a3fd86cfc43c3684e
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-24 17:57:32 +0800

    test

--- litmus/scenarios/mo_io_stress.py
+++ litmus/scenarios/mo_io_stress.py
@@ -7,5 +7,5 @@ class MoIoStress(Base):
     def __init__(self):
         super().__init__()
         self.name = 'mo-io-stress'
-        self.filesystem_utilization_bytes = os.getenv('FILESYSTEM_UTILIZATION_BYTES') or '40g'
+        self.filesystem_utilization_bytes = os.getenv('FILESYSTEM_UTILIZATION_BYTES') or '40'
         self.workers = os.getenv('WORKERS') or '16'
