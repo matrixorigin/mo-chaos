fe56ec46f19c492ad7d11125e280717fff370cae
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-29 17:40:58 +0800

    add data

---
+++ .github/workflows/tke/run-chaos-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+data_scene=$1
+name=$(cat name)
+NAMESPACE=chaos-$name
+kubectl apply -f .github/workflows/tke/$data_scene.yaml -n $NAMESPACE
+until kubectl wait --for=condition=ready pod --selector=job-name=data --timeout=-1s -n $NAMESPACE
+do
+    sleep 5
+done
+kubectl logs -f job.batch/data -n $NAMESPACE
