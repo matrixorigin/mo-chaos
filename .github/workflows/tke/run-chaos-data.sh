209359e8a1b3fede92d765c6d1881a78ff00cea1
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-30 17:31:37 +0800

    fix

--- .github/workflows/tke/run-chaos-data.sh
+++ .github/workflows/tke/run-chaos-data.sh
@@ -6,8 +6,8 @@ name=$(cat name)
 NAMESPACE=chaos-$name
 sed -i "s/\$watch_jobs/$chaos_jobs/g" .github/workflows/tke/$data_scene.yaml
 kubectl apply -f .github/workflows/tke/$data_scene.yaml -n $NAMESPACE
-until kubectl wait --for=condition=ready pod --selector=job-name=data --timeout=-1s -n $NAMESPACE
+until kubectl wait --for=condition=ready pod --selector=job-name=$data_scene --timeout=-1s -n $NAMESPACE
 do
     sleep 5
 done
-kubectl logs -f job.batch/data -n $NAMESPACE
+kubectl logs -f job.batch/$data_scene -n $NAMESPACE
--- .github/workflows/tke/run-chaos.sh
+++ .github/workflows/tke/run-chaos.sh
@@ -4,8 +4,8 @@ exp=$1
 name=$(cat name)
 NAMESPACE=chaos-$name
 kubectl apply -f .github/workflows/tke/$exp.yaml -n $NAMESPACE
-until kubectl wait --for=condition=ready pod --selector=job-name=run --timeout=-1s -n $NAMESPACE
+until kubectl wait --for=condition=ready pod --selector=job-name=$exp --timeout=-1s -n $NAMESPACE
 do
     sleep 5
 done
-kubectl logs -f job.batch/run -n $NAMESPACE
+kubectl logs -f job.batch/$exp -n $NAMESPACE
