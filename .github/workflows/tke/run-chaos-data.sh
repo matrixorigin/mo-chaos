d8d52f053d75e86e7c3ff41f3d0b6ad0dac76f52
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-30 16:34:29 +0800

    add arg

--- .github/workflows/tke/run-chaos-data.sh
+++ .github/workflows/tke/run-chaos-data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
 data_scene=$1
+chaos_jobs=$2
 name=$(cat name)
 NAMESPACE=chaos-$name
+sed -i "s/\$watch_jobs/$chaos_jobs/g" .github/workflows/tke/$data_scene.yaml
 kubectl apply -f .github/workflows/tke/$data_scene.yaml -n $NAMESPACE
 until kubectl wait --for=condition=ready pod --selector=job-name=data --timeout=-1s -n $NAMESPACE
 do
