2a57f22234f28497d6adf1732fbf029dad86ac4c
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-11-30 19:09:34 +0800

    test

--- .github/workflows/tke/run-chaos.sh
+++ .github/workflows/tke/run-chaos.sh
@@ -3,6 +3,10 @@ set -ex
 exp=$1
 name=$(cat name)
 NAMESPACE=chaos-$name
+until kubectl wait --for=condition=ready pod --selector=mo.chaos.data=true --timeout=-1s -n $NAMESPACE
+do
+    sleep 30
+done
 kubectl apply -f .github/workflows/tke/$exp.yaml -n $NAMESPACE
 until kubectl wait --for=condition=ready pod --selector=job-name=$exp --timeout=-1s -n $NAMESPACE
 do
