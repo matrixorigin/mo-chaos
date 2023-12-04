9bbdf45774f489311e3a449f5fc78ce5acb30569
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-12-04 15:18:05 +0800

    test

--- .github/workflows/tke/run-chaos-data.sh
+++ .github/workflows/tke/run-chaos-data.sh
@@ -6,9 +6,9 @@ name=zndmnczhyjclb13e
 NAMESPACE=chaos-$name
 sed -i "s/\$ACTIONS_RUNNER_POD_NAME/$ACTIONS_RUNNER_POD_NAME/" .github/workflows/tke/$data_scene.yaml
 kubectl apply -f .github/workflows/tke/$data_scene.yaml -n $NAMESPACE
-until kubectl wait --for=condition=ready pod --selector=job-name=$data_scene --timeout=-1s -n $NAMESPACE
-do
-    sleep 5
-done
-kubectl logs -f job.batch/$data_scene -n $NAMESPACE
-kubectl delete jobs.batch $data_scene -n $NAMESPACE
+#until kubectl wait --for=condition=ready pod --selector=job-name=$data_scene --timeout=-1s -n $NAMESPACE
+#do
+#    sleep 5
+#done
+#kubectl logs -f job.batch/$data_scene -n $NAMESPACE
+#kubectl delete jobs.batch $data_scene -n $NAMESPACE
