acc70543bbede78cf021257dea4d5e65d200bb2e
Ariznawl@163.com <ariznawl@163.com>
2025-09-12 14:38:12 +0800

    add case

---
+++ config/switch_tpcc_db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+HOST=10.222.6.253
+PORT=6001
+USER=tpcc_test:admin
+PASS=111
+
+# 1. 判断当前该用哪个库
+if mysql -h$HOST -P$PORT -u$USER -p$PASS -e "use tpcc_10_bak; select 1;" 2>/dev/null
+then
+    NEXT_DB=tpcc_10
+    SQL='drop database if exists tpcc_10; create database tpcc_10 clone tpcc_10_bak;'
+else
+    NEXT_DB=tpcc_10_bak
+    SQL='drop database if exists tpcc_10_bak; create database tpcc_10_bak clone tpcc_10;'
+fi
+
+# 2. 执行 clone
+mysql -h$HOST -P$PORT -u$USER -p$PASS -e "$SQL"
+
+# 3. 把软链指向对应 properties 文件
+ln -sf $NEXT_DB.props props.mo      # 保证 props.mo 始终软链到“下一次”要用的库
+
+echo "[$(date)] switched to $NEXT_DB"
